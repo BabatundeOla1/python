d0369ca697b18d763848a53d632903c887af476b
Babatunde <Olaleyebabatun@gmail.com>
2024-09-30 13:00:47 +0100

    phasegate

--- chapter2/chapter2iii.py
+++ chapter2/chapter2iii.py
@@ -5,7 +5,7 @@ firstnumber = userinput // 10000
 secondnumber =  (userinput // 1000) % 10
 
 thirdnumber = (userinput // 100) % 10
-32
+
 fourthnumber = (userinput // 10) % 10
 
 fifthnumber = userinput % 10
---
+++ phasegate1/sum_of_two_integers/tabular_form.py
@@ -0,0 +1,11 @@
+print("a\tb\ta**b")
+
+for count in range(1, 6):
+	a = count
+	
+	b = count + 1
+
+	c = a ** b
+
+	
+	print(a,"\t",b," \t",c)
