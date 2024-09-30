bcf92691339c34391293f3313f3e26def2a19a30
Babatunde <Olaleyebabatun@gmail.com>
2024-09-30 13:58:38 +0100

    phasegate

---
+++ phasegate1/sum_of_two_integers/tabular_form/tabular_form.py
@@ -0,0 +1,11 @@
+print("a\tb\ta**b")
+
+for count in range(1, 6):
+	number = count
+	
+	sum = count + 1
+
+	power = number ** sum
+
+	
+	print(number,"\t",sum," \t",power)
---
+++ phasegate1/sum_of_two_integers/testing/multiplication_of_two_numbers.py
@@ -0,0 +1,16 @@
+
+
+def get_product(first_number, second_number):
+
+	product = 0
+
+	for count in range(second_number):
+
+		product = product + first_number
+
+	return product
+
+
+#print(product)
+
+get_product(9, 2)
