fe8929331db0a5fdb315cb27b8031d2c47f5ee35
Babatunde <Olaleyebabatun@gmail.com>
2024-09-30 13:14:15 +0100

    phasegate

--- phasegate1/sum_of_two_integers/todays_day.py
+++ phasegate1/sum_of_two_integers/todays_day.py
@@ -2,17 +2,46 @@ userinput = int(input("Enter a number for today's day of the week: "))
 
 userinput2 = int(input("Enter a number of days after today's for a future day: "))
 
-0 = Sunday
-1 = Monday
-2 = Tuesday
+if(userinput == 0):
+	userinput = "Sunday"
 
-if(userinput == 0 and userinput2 == 0):
-	print(Sunday)
+if(userinput == 1):
+	userinput = "Monday"
 
-if(userinput == 1 and userinput2 == 1):
-	print(Monday)
+if(userinput == 2):
+	userinput = "Tuesday"
 
-if(userinput == 2 and userinput2 == 2):
-	print(Tuesday)
+if(userinput == 3):
+	userinput = "Wednesday"
+
+if(userinput == 4):
+	userinput = "Thursday"
+
+if(userinput == 5):
+	userinput = "Friday"
+
+if(userinput == 6):
+	userinput = "Saturday"
+
+if(userinput2 == 0):
+	userinput2 = "Sunday"
+
+if(userinput2 == 1):
+	userinput2 = "Monday"
+
+if(userinput2 == 2):
+	userinput2 = "Tuesday"
+
+if(userinput2 == 3):
+	userinput2 = "Wednesday"
+
+if(userinput2 == 4):
+	userinput2 = "Thursday"
+
+if(userinput2 == 5):
+	userinput2 = "Friday"
+
+if(userinput2 == 6):
+	userinput2 = "Saturday"
 
 print("Today's is", userinput, "and the future day is", userinput2)
